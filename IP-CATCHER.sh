256772c8e2c395a211924ef0544b54f4faa0021f
CookieCr2nk <38990807+CookieCr2nk@users.noreply.github.com>
2018-09-27 21:00:07 +0200

    IP-CATCHER.sh Script

---
+++ IP-CATCHER.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script made by https://nebix.tk / Copyright - Noah J.
+# Github-Repository https://github.com/CookieCr2nk/IPDB-Linux
+
+
+echo "-----------------------------------------------------------"
+echo "Herlich Wilkommen beim IP-CATCHER Script."
+echo "Benutzung: Bitte gebe beim nächsten Schritt die gewünschte IP-Adresse ein."
+echo "Script made by https://nebix.tk / Copyright - Noah J."
+echo "-----------------------------------------------------------"
+
+read -p "Geben sie Ihre Gewünschte IP-Adresse ein: " ipadress
+
+curl http://api.db-ip.com/v2/free/$ipadress
+
+exit 1 # Bitte Führe dieses Script als Root aus.
+
+echo "--------------------------------------------"
+echo "Vielen Dank für die Nutzung des Scripts."
+echo "--------------------------------------------"
+
+exit 0
